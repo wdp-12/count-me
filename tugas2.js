8f900a1f1eef4010c40b64b93fcefdddcf348747
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-12 12:35:29 +0700

    first commit

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+function countMe(arr) {
+    let nama = {};
+    for (i = 0; i < arr.length; i++) {
+        let nilai = arr.shift();
+        if (nama[nilai]) {
+            nama[nilai] += 1;
+        } else {
+            nama[nilai] = 1;
+        }
+    }
+    return nama;
+}
+
+
+console.log(countMe(['Sofyan', 'Ricky', 'Sofyan', 'Semmi', 'Semmi', 'Wika']));
+// { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
+
+console.log(countMe([1, 15, 9, 10, 8, 1, 12, 15, 10, 3]));
+// { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
